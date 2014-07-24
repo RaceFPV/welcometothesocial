b52b391d87e72ef79e43bceac1a87ce64b69ca42
slacker87 <racethesunlive@yahoo.com>
2014-07-24 20:59:18 +0000

    allow a user to submit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,23 @@
 class PostsController < ApplicationController
   
   def new
+    @board = Board.find(params[:board_id])
+    @post = @board.posts.new
   end
   
   def create
+    @board = Board.find(params[:board_id])
+    @post = @board.posts.new
+    
+    if @post.save
+      return redirect_to board_post_path(@board.name, @post.id)
+    else
+      return redirect_to new_board_post_path(@board.name)
+    end
   end
   
   def show
+    @board = Board.find(params[:board_id])
   end
   
 end
