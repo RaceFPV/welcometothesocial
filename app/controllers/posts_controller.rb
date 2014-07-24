fa421e75f1dcc6526310340c74b11450b0c062b1
slacker87 <racethesunlive@yahoo.com>
2014-07-24 21:26:14 +0000

    allow new post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     @post = @board.posts.new
-    
+    @post[:title] = params[:postform][:title]
+    @post[:content] = params[:postform][:content]
     if @post.save
       return redirect_to board_post_path(@board.name, @post.id)
     else
@@ -18,6 +19,7 @@ class PostsController < ApplicationController
   
   def show
     @board = Board.find(params[:board_id])
+    @post = @board.posts.find(params[:id])
   end
   
 end
